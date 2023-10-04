27429d81653002696f8c5c3e087bb0006bb9a7e5
kumargourav8427 <kumargourav8427@gmail.com>
2023-10-04 13:06:22 +0530

    datatype conversion

---
+++ Js_basics/conversionOperation.js
@@ -0,0 +1,27 @@
+/*
+let score = "true"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score) ;
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+*/
+
+// "33" => 33
+// "33abc" =>NaN
+// true => 1 ; false  =>0 
+
+let isLoggedIn = "gourav"
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn);
+
+// 1 =>true ; 0 => false
+// "" =>false ; "gourav" =>true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
