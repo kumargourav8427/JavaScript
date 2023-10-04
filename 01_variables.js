6bcc34509715d06b63a9e511aee3442e2ef9675c
kumargourav8427 <kumargourav8427@gmail.com>
2023-10-04 12:08:30 +0530

    hello

--- 01_variables.js
+++ 01_variables.js
@@ -1,8 +1,18 @@
 const accountId =565646;
 let  accountMail = "hgsdhsgg@gmail.com";
 var accountPass = "123164"
-accountCity = "jaipur"
+accountCity = "jaipur"  //posiblity but not used
+let accountState;
 
-accountId = 2;
+// accountId = 2;  //not allowed
 
+accountMail = "yuffys@gmail.com";
+accountPass = "65464";
+accountCity = "ludhiana";
+/*
+prefer do not use var 
+because of issue in block scope and functional scope 
+
+*/
 console.log(accountId);
+console.table([accountId,accountMail,accountPass,accountCity,accountState])
