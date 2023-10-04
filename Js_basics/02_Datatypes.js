1170467325f6354e22ab5a3a1cc73f7c168fd7b6
kumargourav8427 <kumargourav8427@gmail.com>
2023-10-04 12:42:53 +0530

    hello

---
+++ Js_basics/02_Datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //Trat all Js code as newer version
+// alert("hello");  // we are using nodejs, not browser
+let name = "gourav";
+let age = 24 ;
+let isLogged = false
+
+// Number => 2 to power of 53
+// BigInt 
+// String => ""
+// Boolean => True/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+//object
+
+// console.log(typeof(age));
+// console.log(typeof(null)); // it is a object so type is object
+console.log(typeof(undefined));  // undefined
